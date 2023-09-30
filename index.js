abf5e0c8ab9127adc1f8011900fa71d8f2a25cb6
Navya sree Marepalli <marepallinavyasree@gmail.com>
2023-09-30 21:23:34 +0530

    minor changes

--- index.js
+++ index.js
@@ -2,20 +2,35 @@ function convertTemperature() {
   const tempInput = document.getElementById('tempInput').value;
   const fromUnit = document.getElementById('fromUnit').value;
   const toUnit = document.getElementById('toUnit').value;
+  let result;
 
-  if (fromUnit === toUnit) {
-    document.getElementById('result').textContent =
-      'Result: ' + tempInput + ' ' + toUnit;
-    return;
+  if (fromUnit === 'celsius') {
+    if (toUnit === 'fahrenheit') {
+      result = (tempInput * 9) / 5 + 32;
+    } else if (toUnit === 'kelvin') {
+      result = parseFloat(tempInput) + 273.15;
+    } else {
+      result = tempInput; // No conversion needed (from Celsius to Celsius)
+    }
+  } else if (fromUnit === 'fahrenheit') {
+    if (toUnit === 'celsius') {
+      result = ((tempInput - 32) * 5) / 9;
+    } else if (toUnit === 'kelvin') {
+      result = ((tempInput - 32) * 5) / 9 + 273.15;
+    } else {
+      result = tempInput; // No conversion needed (from Fahrenheit to Fahrenheit)
+    }
+  } else if (fromUnit === 'kelvin') {
+    if (toUnit === 'celsius') {
+      result = tempInput - 273.15;
+    } else if (toUnit === 'fahrenheit') {
+      result = ((tempInput - 273.15) * 9) / 5 + 32;
+    } else {
+      result = tempInput; // No conversion needed (from Kelvin to Kelvin)
+    }
   }
 
-  if (fromUnit === 'celsius' && toUnit === 'fahrenheit') {
-    const result = (tempInput * 9) / 5 + 32;
-    document.getElementById('result').textContent =
-      'Result: ' + result.toFixed(2) + ' ' + toUnit;
-  } else if (fromUnit === 'fahrenheit' && toUnit === 'celsius') {
-    const result = ((tempInput - 32) * 5) / 9;
-    document.getElementById('result').textContent =
-      'Result: ' + result.toFixed(2) + ' ' + toUnit;
-  }
+  document.getElementById('result').innerHTML = `Result: ${result.toFixed(
+    2
+  )} ${toUnit}`;
 }
